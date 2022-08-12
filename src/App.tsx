21ca96629bd0f60cb46a200331aa647506d47baf
Royk8 <roy.maestre@udea.edu.co>
2022-08-11 21:34:10 -0500

    Ionic Dojo Ready!

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonLabel>Contactos</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
